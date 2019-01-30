48c85e3fdde4a82c08a2b8ff411baa3660229fb9
RunMyProject <esabatini74@gmail.com>
2019-01-30 09:58:51 +0100

    My First Java Project

---
+++ EsempioJava8/src/com/runmypackage/RunMyClass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.runmypackage;
+
+/**
+ * @author lenovo
+ *
+ */
+public class RunMyClass {
+
+	/**
+	 * 
+	 */
+	public RunMyClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
