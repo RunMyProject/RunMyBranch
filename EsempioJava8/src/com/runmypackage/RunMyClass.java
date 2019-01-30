ade09331f7ecce0b051927f672ddb68a241a3d35
RunMyProject <esabatini74@gmail.com>
2019-01-30 16:35:34 +0100

    Added Stream class example.

--- EsempioJava8/src/com/runmypackage/RunMyClass.java
+++ EsempioJava8/src/com/runmypackage/RunMyClass.java
@@ -3,6 +3,11 @@
  */
 package com.runmypackage;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * @author lenovo
  *
@@ -16,12 +21,35 @@ public class RunMyClass {
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	public static void myPrint(String s) {
+		System.out.println("MyPrint: " + s);
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("TEST Stream");
+		
+		List<String> myList = Arrays.asList("Marte", "Luna", "Terra", "Plutone", "Mercurio", "Giove");
 
+		myList.stream().filter(s -> s.startsWith("M")).map(String::toUpperCase).sorted()
+		.forEach(System.out::println);
+		
+		Arrays.asList("Marte", "Luna", "Terra")
+	    .stream()
+	    .findFirst()
+	    .ifPresent(System.out::println);		
+		
+		Stream.of("Plutone", "Mercurio", "Giove")
+	    .findFirst()
+	    .ifPresent(System.out::println);		
+		
+		IntStream.range(1, 4)
+		.forEach(System.out::println);
 	}
 
 }
